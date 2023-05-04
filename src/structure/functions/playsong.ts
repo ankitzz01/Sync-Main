1698a08d68873ba181a0a0905dc3155c78a9f828
ankitzz <ankitdey450@gmail.com>
2023-05-04 13:55:26 +0530

    CHANGED func playSong

--- src/structure/functions/playsong.ts
+++ src/structure/functions/playsong.ts
@@ -1,6 +1,6 @@
-import { EmbedBuilder, ChatInputCommandInteraction, ModalSubmitInteraction, ButtonInteraction, AnySelectMenuInteraction} from "discord.js"
+import { EmbedBuilder, ChatInputCommandInteraction, ModalSubmitInteraction, ButtonInteraction, AnySelectMenuInteraction } from "discord.js"
 import { Player } from "erela.js"
-import { CustomClient, msToTimestamp } from "../../structure/index.js"
+import { CustomClient, editReply, msToTimestamp } from "../../structure/index.js"
 
 type ValidInteraction = ChatInputCommandInteraction |
     ModalSubmitInteraction |
@@ -9,72 +9,55 @@ type ValidInteraction = ChatInputCommandInteraction |
 
 export async function playSong(interaction: ValidInteraction, client: CustomClient, player: Player, query: string) {
 
-    let res
-
-    if (player.state !== "CONNECTED") player.connect()
-
     try {
 
-        res = await player.search(query, interaction.user)
+        if (player.state !== "CONNECTED") player.connect()
+        let res = await player.search(query, interaction.user)
+        const link = `https://www.google.com/search?q=${encodeURIComponent(res.tracks[0].title)}`
 
-        if (res.loadType === "LOAD_FAILED") {
+        switch (res.loadType) {
+            case "LOAD_FAILED": {
+                if (!player.queue.current) player.destroy()
+                editReply(interaction, "❌", "Something went wrong while playing the requested song")
+            }
+                break;
 
-            if (!player.queue.current) player.destroy()
-            return interaction.editReply({
-                embeds: [new EmbedBuilder()
-                    .setColor("DarkRed")
-                    .setDescription(`An error has occured!`)
-                ]
-            })
+            case "NO_MATCHES": {
+                if (!player.queue.current) player.destroy()
+                editReply(interaction, "❌", "No result found")
+            }
+                break;
 
-        } else if (res.loadType === "NO_MATCHES") {
-            if (!player.queue.current) player.destroy()
-            return interaction.editReply({
-                embeds: [new EmbedBuilder()
-                    .setColor("DarkRed")
-                    .setDescription(`No result found for the query`)
-                ]
-            })
-        } else if (res.loadType === "PLAYLIST_LOADED") {
+            case "PLAYLIST_LOADED": {
+                player.queue.add(res.tracks)
+                if (!player.playing && !player.paused && !player.queue.size) await player.play()
 
-            player.queue.add(res.tracks)
-            if (!player.playing && !player.paused && !player.queue.size) await player.play()
+                interaction.editReply({
+                    embeds: [new EmbedBuilder()
+                        .setColor(client.data.color)
+                        .setAuthor({ name: "ADDED TO QUEUE", iconURL: interaction.user.displayAvatarURL(), url: client.data.links.invite })
+                        .setDescription(`[\`\`${res.tracks[0].title}\`\`](${link})\n\nAdded by: ${interaction.user} | Duration: \`❯ ${msToTimestamp(res.tracks[0].duration)}\``)]
+                })
+            }
+                break;
 
-            let link = `https://www.google.com/search?q=${encodeURIComponent(res.tracks[0].title)}`
+            default: {
+                player.queue.add(res.tracks[0])
+                if (!player.playing && !player.paused && !player.queue.size) await player.play()
 
-            return interaction.editReply({
-                embeds: [new EmbedBuilder()
-                    .setColor(client.data.color)
-                    .setAuthor({ name: "ADDED TO QUEUE", iconURL: interaction.user.displayAvatarURL(), url: client.data.links.invite })
-                    .setDescription(`[\`\`${res.tracks[0].title}\`\`](${link})\n\nAdded by: ${interaction.user} | Duration: \`❯ ${msToTimestamp(res.tracks[0].duration)}\``)
-                ]
-            })
-
-        } else if (["TRACK_LOADED", "SEARCH_RESULT"].includes(res.loadType)) {
-
-            let link = `https://www.google.com/search?q=${encodeURIComponent(res.tracks[0].title)}`
-
-            player.queue.add(res.tracks[0])
-            if (!player.playing && !player.paused && !player.queue.size) await player.play()
-
-            return interaction.editReply({
-                embeds: [new EmbedBuilder()
-                    .setColor(client.data.color)
-                    .setAuthor({ name: "ADDED TO QUEUE", iconURL: interaction.user.displayAvatarURL(), url: client.data.links.invite })
-                    .setDescription(`[\`\`${res.tracks[0].title}\`\`](${link})\n\n**Added by: ${interaction.user} | Duration: **\`\`❯ ${msToTimestamp(res.tracks[0].duration)}\`\``)
-                ]
-            })
+                interaction.editReply({
+                    embeds: [new EmbedBuilder()
+                        .setColor(client.data.color)
+                        .setAuthor({ name: "ADDED TO QUEUE", iconURL: interaction.user.displayAvatarURL(), url: client.data.links.invite })
+                        .setDescription(`[\`\`${res.tracks[0].title}\`\`](${link})\n\n**Added by: ${interaction.user} | Duration: **\`\`❯ ${msToTimestamp(res.tracks[0].duration)}\`\``)
+                    ]
+                })
+            }
+                break;
         }
 
     } catch (error) {
-        interaction.editReply({
-            embeds: [new EmbedBuilder()
-                .setColor("DarkRed")
-                .setDescription(`An **error** has occured! Please report to us using \`/report\`.`)
-            ]
-        })
-        
+        editReply(interaction, "❌", `Something went wrong! Please report to us using \`/report\`.`)
         return console.log(error)
     }
-    
 }
